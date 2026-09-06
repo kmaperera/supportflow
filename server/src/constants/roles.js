8d9d31ef873be5e0e5686b834477302607459b55
kmaperera <malith.perera2021@gmail.com>
2026-09-06 23:55:31 +0530

    add role-based authorization middleware

---
+++ server/src/constants/roles.js
@@ -0,0 +1,7 @@
+﻿const USER_ROLES = Object.freeze({
+  EMPLOYEE: "EMPLOYEE",
+  TECHNICIAN: "TECHNICIAN",
+  ADMIN: "ADMIN",
+});
+
+module.exports = { USER_ROLES };
---
+++ server/src/middleware/authorize.js
@@ -0,0 +1,25 @@
+﻿const ApiError = require("../utils/ApiError");
+const { USER_ROLES } = require("../constants/roles");
+
+function authorizeRoles(...allowedRoles) {
+  if (allowedRoles.length === 0) {
+    throw new TypeError("authorizeRoles requires at least one role");
+  }
+
+  const validRoles = Object.values(USER_ROLES);
+  if (allowedRoles.some((role) => !validRoles.includes(role))) {
+    throw new TypeError("authorizeRoles accepts only EMPLOYEE, TECHNICIAN, or ADMIN");
+  }
+
+  return (req, res, next) => {
+    if (!req.user) {
+      throw new ApiError(401, "Authentication required");
+    }
+    if (!allowedRoles.includes(req.user.role)) {
+      throw new ApiError(403, "You do not have permission to access this resource");
+    }
+    next();
+  };
+}
+
+module.exports = authorizeRoles;
